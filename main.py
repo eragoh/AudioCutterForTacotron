8a1769b2ca9ac80846a72c15519d606e2527a56c
Chlodwig-tech <80710710+Chlodwig-tech@users.noreply.github.com>
2023-09-04 15:15:44 +0200

    Szczepan branch (#2)
    * fixing wav file splitting
    
    * change download directory and silence volume

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from convert_to_wav import convert_to_wav
 from divider import divide
 from wav_to_txt import convert
 
+output_path = 'output_fragments'
 url = input('Podaj youtube url: ')
-name = download_video(url)
-name = convert_to_wav(name)
-dir_path = divide(name)
+name = download_video(url, output_path)
+name = convert_to_wav(name, output_path)
+dir_path = divide(name, output_path)
 convert(dir_path)
